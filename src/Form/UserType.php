faa20bc537f105bff735f175e9b0da15a127aab8
Corentin Maille <maillecorentin@pm.me>
2022-12-09 23:11:24 +0100

    add : add translations for the "my account" section

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,9 +3,12 @@
 namespace App\Form;
 
 use App\Entity\User;
+use PharIo\Manifest\Email;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,11 +17,13 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('email')
-            ->add('password', PasswordType::class, ['attr' => [
-                'value' => '',
+            ->add('firstname', TextType::class, ['label' => 'user.firstname'])
+            ->add('lastname', TextType::class, ['label' => 'user.lastname'])
+            ->add('email', EmailType::class, ['label' => 'user.email'])
+            ->add('password', PasswordType::class, [
+                'label' => 'user.password',
+                'attr' => [
+                    'value' => '',
                 ]
             ])
             ->add('submit', SubmitType::class, [
