10cb453cd580c3e92941fcb5170a3015da3e36e2
Corentin Maille <maillecorentin@pm.me>
2022-12-09 22:46:08 +0100

    - add : Add translations for the product sheet - change : Improved alerts - change : Improvement of the product sheet design - fix : Correction of the "add to cart" functionality from the product page - composer updeate

--- src/Form/CartContentType.php
+++ src/Form/CartContentType.php
@@ -5,9 +5,12 @@ namespace App\Form;
 use App\Entity\CartContent;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Translation\TranslatableMessage;
+use Symfony\Component\Translation\Translator;
 
 class CartContentType extends AbstractType
 {
@@ -15,18 +18,19 @@ class CartContentType extends AbstractType
     {
         $builder
             ->add('quantity', null, [
+                'label' => 'label.quantity',
                 'attr' => [
-                    'class' => 'text-center',
+                    'class' => 'text-center w-25 mx-auto',
                     'min' => 0,
                     'value' => 1
-                ]
+                ],
             ])
             ->add('product', HiddenType::class)
             ->add('submit', SubmitType::class, [
+                'label' => 'cart.button.addTo',
                 'attr' => [
                     'class' => 'text-center mt-3 btn btn-success w-25'
                 ],
-                'label' => 'add to cart'
             ])
         ;
     }
